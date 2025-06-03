81f946275309720b7a45a6e6a9694204e99af1f8
kaalid arare <jaliddd11@gmail.com>
2025-06-02 18:25:47 -0700

    added comments and made small changes. Done!

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,17 +1,19 @@
 import { useState, useEffect } from 'react'
-import { Card, CardContent, Typography, Container, Button, Grid} from "@mui/material";
+import { Card, CardContent, Typography, Button, Grid} from "@mui/material";
 
 import './App.css'
 
+// Fecthed the pokemons from the Pokemon API, stored them in a state and displaying them in the DOM
 function App() {
 
   const [pokemons, setPokemons] = useState([]);
-  const [offset, setOffset] = useState(0);
+  const [offSet, setOffSet] = useState(0);
   const [selectedPokemon, setSelectedPokemon] = useState(null);
 
+  // useEffect retrieves a set of new pokemons each time offSet changes
   useEffect(() => {
     fetchPokemons();
-  }, [offset])
+  }, [offSet])
 
   const fetchPokemonDetails = async (url) => {
     const res = await fetch(url);
@@ -19,22 +21,22 @@ function App() {
     console.log(data);
     setSelectedPokemon(data);
   }
-
+  // This function retrieves 20 pokemons from the API
   const fetchPokemons = async () => {
-    const res = await fetch(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=20`);
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon?offset=${offSet}&limit=20`);
     const data = await res.json();
     console.log(data);
     setPokemons(data.results);
   }
 
   const handleNext = () => {
-    setOffset((prevOffset) => prevOffset + 20);
+    setOffSet((prevOffset) => prevOffset + 20);
     setSelectedPokemon(null);
   };
 
   const handleBack = () => {
-    if(offset > 0) {
-      setOffset((prevOffset) => prevOffset - 20);
+    if(offSet > 0) {
+      setOffSet((prevOffset) => prevOffset - 20);
       setSelectedPokemon(null);
     }
   }
@@ -44,7 +46,7 @@ function App() {
       <Typography variant="h3" gutterBottom color='white'>
         Pokémon List
       </Typography>
-
+      {/**Step 3: Displaying the returned pokemon cards in a 5x4 grid and styling it using Material UI sx*/}
       <Grid container spacing={2} justifyContent="center"
        sx={{ 
         paddingTop:"20px", 
@@ -81,12 +83,12 @@ function App() {
             </Grid>
           ))}
       </Grid>
-
+      {/**The next button fetches new 20 pokemon cards while back button fetches the 20 previous pokemon cards */}
       <div style={{ marginTop: "20px"}}>
         <Button
          
           onClick={handleBack}
-          disabled={offset === 0}
+          disabled={offSet === 0}
           sx={{ marginRight: 2, backgroundColor:"gray", color:"black"}}
         >
           Back
@@ -108,7 +110,7 @@ function App() {
         </Button>
       </div>
 
-      {/** Step 4: Display selected Pokémon details in a styled card */}
+      {/** Step 4: Displaying the selected Pokémon details in a styled card using MUI*/}
       {selectedPokemon && (
         <Card sx={{mt: 4, padding:"1px", marginLeft:"500px", marginRight:"500px", backgroundColor:"darkgray"}}>
           <CardContent>
